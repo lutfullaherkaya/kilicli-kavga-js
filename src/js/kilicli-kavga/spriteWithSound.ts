ec55f2f5fa7187e88dd8d54ce9afe7f36991e739
lutfu <lerkaya0@gmail.com>
2022-09-04 14:00:35 +0300

    fix(ses): chrome ses loopu düzeltildi.

--- src/js/kilicli-kavga/spriteWithSound.ts
+++ src/js/kilicli-kavga/spriteWithSound.ts
@@ -1,10 +1,16 @@
 import {Tuval} from "@/js/kilicli-kavga/tuval";
 import {Sprite} from "@/js/kilicli-kavga/sprite";
 
+function mobilMi(): boolean {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 export class SpriteWithSound extends Sprite {
     private sound: HTMLAudioElement | null = null;
     private readonly soundLoops
     private playedOnce = false;
+    private mobileChromeLoopDelayInSeconds = 0.3; // in chrome mobile, sound loops with delay. So we loop early.
+
 
     constructor(tuval: Tuval, {
         soundSrc = '',
@@ -15,9 +21,6 @@ export class SpriteWithSound extends Sprite {
         this.soundLoops = soundLoops;
         if (soundSrc) {
             this.sound = new Audio(soundSrc);
-            /*if (soundLoops) {
-                this.sound.loop = true; // we can't do this since chrome android loops with delay.
-            }*/
         }
     }
 
@@ -42,6 +45,13 @@ export class SpriteWithSound extends Sprite {
     start(): this {
         super.start();
         if (this.sound && (!this.playedOnce || this.soundLoops)) {
+            let loopRewindTime = this.sound.duration;
+            if (mobilMi()) {
+                loopRewindTime -= this.mobileChromeLoopDelayInSeconds;
+            }
+            if (this.soundLoops && this.sound.currentTime >= loopRewindTime) {
+                this.sound.currentTime = 0;
+            }
             this.sound.play().then(() => {
                 this.playedOnce = true;
             });
@@ -53,22 +63,12 @@ export class SpriteWithSound extends Sprite {
         super.pause();
         if (this.sound) {
             this.sound.pause();
-            console.log(this.sound.duration)
-
         }
         return this;
     }
 
     update() {
         super.update();
-        if (this.sound) {
-            if (this.soundIsEnded() && this.soundLoops && this.isPlaying) {
-                /*this.sound.currentTime = 0;
-                this.sound.play();*/
-                this.sound = new Audio(this.sound.src); // this is neccesary for chrome android. Otherwise, sound will loop with delay.
-
-            }
-        }
         return this;
     }
 
