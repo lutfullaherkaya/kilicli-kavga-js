f94157340c5c41bf8a1432d5c30cc77adf2a8cc2
lutfu <lerkaya0@gmail.com>
2024-08-22 00:17:35 +0300

    vue 3 ve prime vue ekle

---
+++ frontend/src/main.ts
@@ -0,0 +1,25 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+const app = createApp(App)
+
+app.use(router)
+
+app.use(PrimeVue, { 
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+          name: 'primevue',
+          order: 'tailwind-base, primevue, tailwind-utilities'
+      }
+  }
+  }
+ });
+
+app.mount('#app')
